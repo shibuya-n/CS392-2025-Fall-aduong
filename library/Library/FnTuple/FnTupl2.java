292ca3e8d0c5c12aec937a207df6365638b9ca5c
Hongwei <gmhwxi@gmail.com>
2025-09-14 15:28:55 -0400

    Updating: very very minorly

--- library/Library/FnTuple/FnTupl2.java
+++ library/Library/FnTuple/FnTupl2.java
@@ -5,4 +5,11 @@ public class FnTupl2<T0,T1> {
     FnTupl2(T0 x0, T1 x1) {
 	sub0 = x0; sub1 = x1;
     }
-}
+
+    public void System$out$print() {
+	FnTupl2Util.System$out$print(this);
+    }
+    public String toString() {
+	return "FnTupl2(" + sub0.toString() + "," + sub1.toString() + ")";
+    }
+} // end of [public class FnTupl2<T0,T1>{...}]
--- library/Library/FnTuple/FnTupl3.java
+++ library/Library/FnTuple/FnTupl3.java
@@ -6,4 +6,10 @@ public class FnTupl3<T0,T1,T2> {
     FnTupl3(T0 x0, T1 x1, T2 x2) {
 	sub0 = x0; sub1 = x1; sub2 = x2;
     }
-}
+    public void System$out$print() {
+	FnTupl3Util.System$out$print(this);
+    }
+    public String toString() {
+	return "FnTupl3(" + sub0.toString() + "," + sub1.toString() + "," + sub2.toString() + ")";
+    }
+} // end of [public class FnTupl3<T0,T1,T2>{...}]
