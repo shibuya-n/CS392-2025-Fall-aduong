3d8911221be360bfdfa47c2882e32a63a2ac595d
Hongwei <gmhwxi@gmail.com>
2025-10-16 14:01:06 -0400

    Updating: very very minorly

---
+++ quizzes/01/Code/Quiz01_02.java
@@ -0,0 +1,28 @@
+import Library.FnList.*;
+import java.util.function.Consumer;
+//
+// HX: 30 points
+// This one tests your understanding of higher-order
+// methods. Trying to construct a consumer of consumers
+// (of the type Consumer<Consumer<Character>>) can help
+// you understand the meaning of this one.
+//
+public class Quiz01_02 {
+    public static
+	FnList<Character>
+	thirdOrderFun
+	(Consumer<Consumer<Character>> ffcs) {
+	// HX: Given a consumer of consumers of characters,
+	// thirdOrderFun returns a string cs.
+	// Given fcs = (ch) -> System.out.print(ch),
+	// which is of the type Consumer<Character>,
+	// ffcs.accept(fcs) and cs.foritm(fcs) should behave
+	// the same.
+	return new FnList<Character>();
+    }
+    public static void main (String[] args) {
+	// HX-2025-10-12:
+	// Please write minimal testing code for thirdOrderFun.
+	return /*void*/;
+    }
+}
---
+++ quizzes/01/Code/Quiz01_06.java
@@ -0,0 +1,35 @@
+//
+// HX: 50 points
+//
+abstract public class Quiz01_06 {
+    public static<T>
+	FnList<T> someSort
+	(FnList<T> xs, ToIntBiFunction<T,T> cmp) {
+	// HX-2025-10-15:
+	// This one is abstract, that is, not implemented
+    }
+    public static
+	<T extends Comparable<T>>
+	FnList<T> someStableSort
+	(FnList<T> xs, ToIntBiFunction<T,T> cmp) {
+	// HX-2025-10-15:
+	// Please implement a stable sorting method based on
+	// someSort, which may not be stable
+    }
+}
+
+////////////////////////////////////////////////////////////////////////.
+//
+// HX-2025-10-15:
+// Please find a way to test someStableSort by
+// implementing someSort as quickSort on FnList
+// and then use someStableSort to parity-sort the following
+// list of 1M integers:
+// 0, 1, 2, 3, 4, ..., 999999
+// That is, the result of parity-sorting should be:
+// 0, 2, ..., 999998, 1, 3, ..., 999999
+//
+// Note that you should be able to call the quickSort method
+// in Library/FnList/FnListSUtil.java; should not do another
+// implementation of quickSort in your testing code.
+////////////////////////////////////////////////////////////////////////.
