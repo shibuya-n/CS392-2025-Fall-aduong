a20aa4b1a4fc8724620ef6958b095e9f382338b8
shibuya-n <109704872+shibuya-n@users.noreply.github.com>
2025-11-11 17:28:24 -0500

    update

--- libraries/Library/FnList/FnListUtil.java
+++ libraries/Library/FnList/FnListUtil.java
@@ -2,222 +2,31 @@ package Library.FnList;
 
 import Library.FnGseq.*;
 
-<<<<<<< HEAD
-public class FnListUtil {
-	//
-	public static <T> boolean nilq(FnList<T> xs) {
-		return xs.nilq();
-	}
-
-	public static <T> boolean consq(FnList<T> xs) {
-		return xs.consq();
-	}
-
-	//
-	public static <T> void System$out$print(FnList<T> xs) {
-		System.out.print("FnList(");
-		FnListUtil.iforitm(xs,
-				(i, itm) -> {
-					if (i > 0) {
-						System.out.print(",");
-					}
-					System.out.print(itm.toString());
-				});
-		System.out.print(")");
-	}
-
-	//
-	public static FnList<Integer> list_make_int1(int n0) {
-		FnList<Integer> xs = new FnList<Integer>();
-		for (int i0 = n0 - 1; i0 >= 0; i0 -= 1) {
-			xs = new FnList<Integer>(i0, xs);
-		}
-		return xs;
-	}
-
-	//
-	public static <T, R> FnList<R> map_list(FnList<T> xs, Function<? super T, R> fopr) {
-		FnList<R> res = new FnList<R>();
-		while (true) {
-			if (xs.nilq())
-				break;
-			res = new FnList<R>(fopr.apply(xs.hd()), res);
-			xs = xs.tl();
-		}
-		return res.reverse();
-	}
-
-	public static <T, R> FnList<R> rmap_list(FnList<T> xs, Function<? super T, R> fopr) {
-		FnList<R> res = new FnList<R>();
-		xs = xs.reverse();
-		while (true) {
-			if (xs.nilq())
-				break;
-			res = new FnList<R>(fopr.apply(xs.hd()), res);
-			xs = xs.tl();
-		}
-		return res.reverse();
-	}
-
-	public static <T, R> FnList<R> imap_list(FnList<T> xs, BiFunction<Integer, ? super T, R> fopr) {
-		int i0 = 0;
-		FnList<R> res = new FnList<R>();
-		while (true) {
-			if (xs.nilq())
-				break;
-			res = new FnList<R>(fopr.apply(i0, xs.hd()), res);
-			i0 += 1;
-			xs = xs.tl();
-		}
-		return res.reverse();
-	}
-
-	public static <T, R> FnList<R> irmap_list(FnList<T> xs, BiFunction<Integer, ? super T, R> fopr) {
-		int i0 = 0;
-		FnList<R> res = new FnList<R>();
-		xs = xs.reverse();
-		while (true) {
-			if (xs.nilq())
-				break;
-			res = new FnList<R>(fopr.apply(i0, xs.hd()), res);
-			i0 += 1;
-			xs = xs.tl();
-		}
-		return res.reverse();
-	}
-
-	//
-	public static <T> void foritm(FnList<T> xs, Consumer<? super T> action) {
-		xs.foritm(action);
-		return;
-	}
-
-	public static <T> void rforitm(FnList<T> xs, Consumer<? super T> action) {
-		xs.reverse().foritm(action);
-		return;
-	}
-
-	public static <T> void iforitm(FnList<T> xs, BiConsumer<Integer, ? super T> action) {
-		xs.iforitm(action);
-		return;
-	}
-
-	public static <T> void irforitm(FnList<T> xs, BiConsumer<Integer, ? super T> action) {
-		xs.reverse().iforitm(action);
-		return;
-	}
-
-	//
-	public static <T> boolean forall(FnList<T> xs, Predicate<? super T> pred) {
-		return xs.forall(pred);
-	}
-
-	public static <T> boolean rforall(FnList<T> xs, Predicate<? super T> pred) {
-		return xs.reverse().forall(pred);
-	}
-
-	public static <T> boolean iforall(FnList<T> xs, BiPredicate<Integer, ? super T> pred) {
-		return xs.iforall(pred);
-	}
-
-	public static <T> boolean irforall(FnList<T> xs, BiPredicate<Integer, ? super T> pred) {
-		return xs.reverse().iforall(pred);
-	}
-
-	//
-	public static <T> FnList<T> reverse(FnList<T> xs) {
-		FnList<T> r0 = new FnList<T>();
-		return FnListUtil.folditm(xs, r0, (r1, x1) -> new FnList<T>(x1, r1));
-	}
-
-	//
-	public static <T> FnList<T> rappend(FnList<T> xs, FnList<T> ys) {
-		return FnListUtil.folditm(xs, ys, (r1, x1) -> new FnList<T>(x1, r1));
-	}
-
-	//
-	public static <T, R> R folditm(FnList<T> xs, R r0, BiFunction<R, ? super T, R> fopr) {
-		R res = r0;
-		while (true) {
-			if (xs.nilq())
-				break;
-			res = fopr.apply(res, xs.hd());
-			xs = xs.tl();
-		}
-		return res;
-	}
-
-	//
-	public static <T, R> R rfolditm(FnList<T> xs, R r0, BiFunction<? super T, R, R> fopr) {
-		return FnListUtil.folditm(xs.reverse(), r0, (x1, r1) -> fopr.apply(r1, x1));
-	}
-
-	public static FnList<Integer> insertSort(FnList<Integer> xs) {
-		if (xs.nilq()) {
-			return xs;
-		} else {
-			return insert(xs.hd(), insertSort(xs.tl()));
-		}
-	}
-
-	private static FnList<Integer> insert(Integer x0, FnList<Integer> xs) {
-		if (xs.nilq()) {
-			return new FnList<Integer>(x0, xs); // [x0] is returned
-		} else {
-			if (x0 <= xs.hd()) {
-				return new FnList<Integer>(x0, xs);
-			} else {
-				return new FnList<Integer>(xs.hd(), insert(x0, xs.tl()));
-			}
-		}
-	}
-
-	private static <T extends Comparable<T>> FnList<T> mergeSort(FnList<T> xs) {
-		return mergeSort(xs, (x1, x2) -> x1.compareTo(x2));
-	}
-
-	public static<T> FnList<T> mergeSort(FnList<T> xs, ToIntBiFunction<T, T> cmp){ 
-		if (xs.length() <= 1){ 
-			return xs; 
-		}
-		else { 
-			split(xs, nil(), xs.length(), 0, cmp); 
-		}
-	}
-
-	private static<T> FnList<T> split(FnList<T> xs, FnList<T> ys, int n1, int n2 ToIntBiFunction<T, T> cmp) {
-		if (n2 +n2 < n1){ 
-			split(xs.tl(), cons(xs.hd(), ys), n0, n1+1, cmp); 
-		} else { 
-			mergeSort(reverse(ys), cmp, mergeSort(xs, cmp)); 
-		}
-	} 
-
-	//
-}
-// end of [public class FnListUtil{...}]
-=======
 import java.util.function.Consumer;
 
 public class FnListUtil<X0>
-    extends FnGseq<FnList<X0>,X0> {
-//
-    @Override
-    public FnList<X0>
-    list$make(FnList<X0> xs) { return xs; }
+		extends FnGseq<FnList<X0>, X0> {
+	//
+	@Override
+	public FnList<X0> list$make(FnList<X0> xs) {
+		return xs;
+	}
 
-    @Override
-    public FnList<X0>
-	listize(FnList<X0> xs) { return xs; }
-    @Override
-    public FnList<X0>
-	rlistize(FnList<X0> xs) { return xs.reverse(); }
-//
-    @Override
-    public void
-	foritm(FnList<X0> xs, Consumer<? super X0> work) {
-	xs.foritm(work); return /*void*/;
-    }
-//
+	@Override
+	public FnList<X0> listize(FnList<X0> xs) {
+		return xs;
+	}
+
+	@Override
+	public FnList<X0> rlistize(FnList<X0> xs) {
+		return xs.reverse();
+	}
+
+	//
+	@Override
+	public void foritm(FnList<X0> xs, Consumer<? super X0> work) {
+		xs.foritm(work);
+		return /* void */;
+	}
+	//
 } // end of [public class FnListUtil<X0>{...}]
->>>>>>> upstream/main
